574496aa6d9ba81617d281ff7ce0183becf5e1c8
nguyenthao71005 <nguyenthao7102005@gmail.com>
2026-01-01 17:08:46 +0700

    Delete Lab02/TL directory

--- Lab02/TL/01_variables.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Nguyễn Phương Thảo<br>";
-echo "DCCNTT14.2<br>";
-echo "nguyenthao7102005@gmail.com<br>";
-?>
--- Lab02/TL/02_operators_math.php
+++
@@ -1,8 +0,0 @@
-<?php
-$r = 10;
-$pi = 3.14;
-$chuvi = 2 * $pi * $r;
-$dientich = $pi * $r * $r;
-echo "Diện tích:  $dientich <br>";
-echo "Chu vi: $chuvi" 
-?>
